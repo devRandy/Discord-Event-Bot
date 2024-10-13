19e32afa07b51a74f53bc11daa7baa8aff30564d
ccraven9 <c.craven1337@gmail.com>
2024-10-13 18:45:32 -0400

    Rename commands.js to showCommands.js

--- src/commands/utility/showCommands.js
+++ src/commands/utility/showCommands.js
@@ -1,13 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('commands')
 		.setDescription('Lists all avalible commands!'),
 	async execute(interaction) {
 		await interaction.reply(`/commands,
-			/ping`);
+		/ping`);
 	},
 };
