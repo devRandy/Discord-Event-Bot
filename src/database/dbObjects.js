fbbc5cd6643d74cd4b8fb440e694f28f0280b721
ccraven9 <c.craven1337@gmail.com>
2024-10-23 21:45:47 -0400

    Remove unneeded classes

--- src/database/dbObjects.js
+++ src/database/dbObjects.js
@@ -8,54 +8,53 @@ const sequelize = new Sequelize('database', 'username', 'password', {
 });
 
 const Users = require('./models/users.js')(sequelize, Sequelize.DataTypes);
-// const CurrencyShop = require('./models/shop.js')(sequelize, Sequelize.DataTypes);
 const UserInventory = require('./models/user-inventory.js')(sequelize, Sequelize.DataTypes);
 const Cards = require('./models/card-model.js')(sequelize, Sequelize.DataTypes);
 
 UserInventory.belongsTo(Cards, { foreignKey: 'card_id', as: 'card' });
 
-try {
-    Reflect.defineProperty(Users.prototype, 'addCard', {
-        value: async card => {
-            const userItem = await UserInventory.findOne({
-                where: { user_id: this.user_id, card_id: card.id },
-            });
+// try {
+//     Reflect.defineProperty(Users.prototype, 'addCard', {
+//         value: async card => {
+//             const userItem = await UserInventory.findOne({
+//                 where: { user_id: this.user_id, card_id: card.id },
+//             });
     
-            if (userItem) {
-                userItem.amount += 1;
-                return userItem.save();
-            }
+//             if (userItem) {
+//                 userItem.amount += 1;
+//                 return userItem.save();
+//             }
     
-            return UserInventory.create({ user_id: this.user_id, card_id: card.id, amount: 1 });
-        },
-    });
+//             return UserInventory.create({ user_id: this.user_id, card_id: card.id, amount: 1 });
+//         },
+//     });
     
-    Reflect.defineProperty(Users.prototype, 'getCards', {
-        value: () => {
-            return UserInventory.findAll({
-                where: { user_id: this.user_id },
-                include: ['card'],
-            });
-        },
-    });
+//     Reflect.defineProperty(Users.prototype, 'getCards', {
+//         value: () => {
+//             return UserInventory.findAll({
+//                 where: { user_id: this.user_id },
+//                 include: ['card'],
+//             });
+//         },
+//     });
 
-    Reflect.defineProperty(Cards.prototype, 'getCommonCards', {
-        value: () => {
-            return Cards.findAll({
-                where: {card_rarity: 'Common' },
-            });
-        },
-    });
+//     Reflect.defineProperty(Cards.prototype, 'getCommonCards', {
+//         value: () => {
+//             return Cards.findAll({
+//                 where: {card_rarity: 'Common' },
+//             });
+//         },
+//     });
 
 
 
   
 
-    console.log('Succseful Database Sync');
+//     console.log('Succseful Database Sync');
 
-} catch (error) {
-    console.log(error)
-}
+// } catch (error) {
+//     console.log(error)
+// }
 
 
 
